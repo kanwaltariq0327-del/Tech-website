ce29de56ae104f6825e63f0ce12c316d25598440
kanwaltariq0327-del <kanwal.tariq0327@gmail.com>
2026-06-07 19:31:08 +0500

    pricing tab changes

--- html, css files/features.js
+++ html, css files/features.js
@@ -1,19 +1,16 @@
 const featuresButtons = document.querySelectorAll("#button");
-
-
-const arrow = document.querySelector(".down-arrow")
-
+const arrow = document.querySelector(".down-arrow");
 
 arrow.addEventListener("click", () => {
-    featuresButtons.forEach(button => {
-        button.classList.toggle("show-buttons" && "active");
-        console.log(button.classList.toggle("show-buttons"));
-    })
-})
+  featuresButtons.forEach((button) => {
+    button.classList.toggle("show-buttons" && "active");
+    console.log(button.classList.toggle("show-buttons"));
+  });
+});
 
 const parent = document.querySelector(".features-mid");
 const btn = document.querySelector("#button");
 
-btn.addEventListener('click', () => {
-    parent.prepend(btn);
-})
+btn.addEventListener("click", () => {
+  parent.prepend(btn);
+});
